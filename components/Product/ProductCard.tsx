7a4c038267f1246c9bc0ef16b3b64f04ad853b7c
unkuz <hi.unkuz@gmail.com>
2024-07-19 00:25:17 +0700

    🍍

--- components/Product/ProductCard.tsx
+++ components/Product/ProductCard.tsx
@@ -25,7 +25,7 @@ export const ProductCard = ({
 
   const description = useMemo(() => {
     return `${productFull} ${duration} - ${type}`
-  }, [])
+  }, [productFull, duration, type])
 
   useEffect(() => {
     VanillaTilt.init(elRef.current!, {
@@ -37,10 +37,10 @@ export const ProductCard = ({
   }, [])
   return (
     <>
-    <Link href={`/product/${Math.random()}`}>
-      <div
-        ref={elRef}
-        className="w-full relative bg-[#c2fbff] rounded-[20px]
+      <Link href={`/product/${Math.random()}`}>
+        <div
+          ref={elRef}
+          className="w-full relative bg-[#c2fbff] rounded-[20px]
     before:content-[''] before:absolute before:top-[-3px] before:left-[-3px] before:right-[-3px]
     before:bottom-[-3px] before:border-[3px] before:border-[#61e7ff] before:animate-clippath
     before:rounded-[25px] after:content-[''] after:absolute after:top-[-3px] after:left-[-3px]
@@ -56,23 +56,23 @@ export const ProductCard = ({
     hover:z-[1]
     cursor-pointer
     "
-      >
-        <div className="w-full aspect-[2.3/1] bg-[#aeecff] backdrop-blur-lg rounded-[15px] relative p-[20px] flex justify-between">
-          <div>
-            <p className="font-bold text-[25px]">Gói gia hạn</p>
-            <p className="font-extrabold text-[50px] text-[#000000]">{product}</p>
-            <p className="font-bold text-[25px]">{duration}</p>
+        >
+          <div className="w-full aspect-[2.3/1] bg-[#aeecff] backdrop-blur-lg rounded-[15px] relative p-[20px] flex justify-between">
+            <div>
+              <p className="font-bold text-[25px]">Gói gia hạn</p>
+              <p className="font-extrabold text-[50px] text-[#000000]">{product}</p>
+              <p className="font-bold text-[25px]">{duration}</p>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: svg }} className="w-[150px]"></div>
           </div>
-          <div dangerouslySetInnerHTML={{ __html: svg }} className="w-[150px]"></div>
+          <div className="mt-[5px]">
+            <p className="font-bold">{description}</p>
+            <p className="font-bold flex gap-[10px] items-center">
+              <span className="text-[22px] text-[#000000] font-extrabold">{sale}.000đ</span>
+              <span className="line-through text-[#858585] blur-sm">{preSale}.000đ</span>
+            </p>
+          </div>
         </div>
-        <div className="mt-[5px]">
-          <p className="font-bold">{description}</p>
-          <p className="font-bold flex gap-[10px] items-center">
-            <span className="text-[22px] text-[#000000] font-extrabold">{sale}.000đ</span>
-            <span className="line-through text-[#858585] blur-sm">{preSale}.000đ</span>
-          </p>
-        </div>
-      </div>
       </Link>
     </>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from '@next/bundle-analyzer'
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default withBundleAnalyzer(nextConfig);
