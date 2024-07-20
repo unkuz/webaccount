a5a25774650d390f2a928fb10a7a5c8ab9bf8a7b
unkuz <hi.unkuz@gmail.com>
2024-07-20 09:30:55 +0700

    🍍

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,13 +31,13 @@ export const Hero = () => {
       className="keen-slider relative aspect-[2.5/1] rounded-[20px] text-[1.5rem] shadow-xl *:flex *:items-center *:justify-center *:text-black sm:!aspect-[3.5/1] sm:text-[2rem] lg:text-[3.5rem]"
     >
       <div className="keen-slider__slide bg-[#93ec58] px-[20px] font-bold">
-        <p className="text-center">Web bán Account Premium giá rẻ</p>
+        <p className="text-center">Web bán Account Premium</p>
+      </div>
+      <div className="keen-slider__slide bg-[#9f69e7] px-[20px] font-bold">
+        <p className="text-center">Giá sao cũng bán</p>
       </div>
       <div className="keen-slider__slide bg-[#58dbec] px-[20px] font-bold">
         <p className="text-center">Uy tín, chất lượng, bảo hành trọn thời gian sử dụng</p>
-      </div>
-      <div className="keen-slider__slide bg-[#9f69e7] px-[20px] font-bold">
-        <p className="text-center">Ở đâu bán rẻ, chúng tôi còn bán rẻ hơn !</p>
       </div>
     </div>
   )
