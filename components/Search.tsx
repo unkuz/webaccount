937cbbeacc54464db8a53138bc6a4e11a47235c4
unkuz <hi.unkuz@gmail.com>
2024-07-21 17:56:37 +0700

    🍕

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,7 +5,7 @@ export const Search = () => {
   return (
     <div className="flex h-[55px] w-[500px] overflow-hidden rounded-[15px] border-[2px] border-[#29dfff]">
       <input
-        className="h-full w-[400px] flex-1 bg-transparent px-[20px] font-semibold caret-[#00e1ff] focus:outline-none"
+        className="h-full w-[400px] flex-1 cursor-text bg-transparent px-[20px] font-semibold caret-[#00e1ff] focus:outline-none"
         placeholder="Tìm kiếm"
       />
       <Image src={SearchIcon} alt="Search Icon" className="w-[50px] cursor-pointer px-[10px]" />
